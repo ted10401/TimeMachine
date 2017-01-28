87dd340c010c85c5294767104b3add4ab9a98e76
ted <ted10401@gmail.com>
2017-01-28 16:32:44 +0800

    - Implemented the particle system rewind.

--- Assets/Scripts/Timeline/BaseTimeline.cs
+++ Assets/Scripts/Timeline/BaseTimeline.cs
@@ -5,12 +5,16 @@ public abstract class BaseTimeline : MonoBehaviour, ITimeline
     private void Awake()
     {
         TimelineManager.Instance.RegisterTimeline(this);
+
+        Initialize();
     }
 
     private void OnDestroy()
     {
         TimelineManager.Instance.UnregisterTimeline(this);
     }
+
+    public virtual void Initialize(){}
 
     public abstract void AddTime(float deltaTime);
 }
---
+++ Assets/Scripts/Timeline/ParticleSystemTimeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemTimeline : BaseTimeline
+{
+    private ParticleSystem _particleSystem;
+    private float _time;
+
+    public override void Initialize()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+        _particleSystem.randomSeed = 1;
+    }
+
+
+    public override void AddTime(float deltaTime)
+    {
+        _time += deltaTime;
+
+        _particleSystem.Simulate(_time);
+    }
+}
